d71ccaf764ed8b768ecf5384ec7fa5e1f4461aa0
quietQuiet <j.lietunovas@gmail.com>
2019-01-09 14:37:51 +0100

    create elements with data from user input

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -10,11 +10,31 @@
 
 // Add an event (name and comment) listener/handler to the button
 function submitComment() {
-    console.log('Hello');
-}
-// Gather data from the input fields
+    // Gather data from the input fields via .value
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
 
-// Create elements to display that data
+    // Create elements to display that data 
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    // use the previously gathered .values and add them to the newly created 
+    // elements
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg;
+
+    // adding a class to the section element
+    comment.classList.add('comment');
+    // appending two kids (h3 and p) to the section element
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment);
+}
+
+
+
 
 // Display the created elements with data
 
